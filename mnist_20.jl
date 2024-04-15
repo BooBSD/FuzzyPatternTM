c0bbaab4171f4129f485835e2ee57456a5e999f1
Artem <boobsd@gmail.com>
2024-04-14 23:28:05 -0700

    Added Testing different LF on trained model

--- mnist_20.jl
+++ mnist_20.jl
@@ -7,8 +7,9 @@ catch LoadError
     Pkg.add("MLDatasets")
 end
 
+using Printf: @printf
 using MLDatasets: MNIST, FashionMNIST
-using .FuzzyPatternsTM: TMInput, TMClassifier, train!, save, load, unzip
+using .FuzzyPatternsTM: TMInput, TMClassifier, train!, predict, accuracy, save, load, unzip
 
 
 x_train, y_train = unzip([MNIST(:train)...])
@@ -43,3 +44,11 @@ tm = TMClassifier(CLAUSES, T, R, L=L, LF=LF, states_num=256, include_limit=200) 
 tm_best, _ = train!(tm, x_train, y_train, x_test, y_test, EPOCHS, shuffle=true, verbose=2)
 
 save(tm_best, "/tmp/tm_20.tm")
+tm = load("/tmp/tm_20.tm")
+
+println("Testing different LF on trained model:\n")
+for i in 0:100
+    tm.LF = i
+    @printf("LF = %s, Accuracy: %.2f%%", i, accuracy(predict(tm, x_test), y_test) * 100)
+    println(LF == i ? "  <== This is original model LF" : "")
+end
