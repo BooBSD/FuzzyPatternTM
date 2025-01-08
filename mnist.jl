7b51e6c752e4e7e6f1fa0b7db31fc9d170efa8f7
Artem <boobsd@gmail.com>
2025-01-08 05:06:47 -0800

    MNIST example: switch to `batch=true` by default

--- mnist.jl
+++ mnist.jl
@@ -48,14 +48,13 @@ const best_tms_size = 512
 
 # Training the TM model
 tm = TMClassifier{eltype(y_train)}(CLAUSES, T, R, L=L, LF=LF, states_num=256, include_limit=200)  # include_limit=200 instead of 128 but you can try different numbers.
-# Batch inference is not implemented because of new algorithm.
-tms = train!(tm, x_train, y_train, x_test, y_test, EPOCHS, best_tms_size=best_tms_size, shuffle=true, batch=false, verbose=1)
+tms = train!(tm, x_train, y_train, x_test, y_test, EPOCHS, best_tms_size=best_tms_size, shuffle=true, batch=true, verbose=1)
 
 save(tms, "/tmp/tms.tm")
 tms = load("/tmp/tms.tm")
 
 # Binomial combinatorial merge of trained TM models
-tm, _ = combine(tms, 2, x_test, y_test, batch=false)
+tm, _ = combine(tms, 2, x_test, y_test, batch=true)
 save(tm, "/tmp/tm2.tm")
 tm = load("/tmp/tm2.tm")
 
@@ -64,4 +63,4 @@ optimize!(tm, x_train)
 save(tm, "/tmp/tm_optimized.tm")
 tm_opt = load("/tmp/tm_optimized.tm")
 
-benchmark(tm_opt, x_test, y_test, 5000, batch=false, warmup=true)
+benchmark(tm_opt, x_test, y_test, 5000, batch=true, warmup=true)
